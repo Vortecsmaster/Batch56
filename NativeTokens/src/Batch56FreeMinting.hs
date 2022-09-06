2b1b7e18a7702492d070cb429b6c3fc061c76183
Vortecsmaster <vortecsmaster@vortecsla.com>
2022-09-06 05:40:40 -0500

    Session 2022-09-06 Scaffolding files for NativeTokens session.

---
+++ NativeTokens/src/Batch56FreeMinting.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Batch56FreeMinting where
